21449637952a5b5e2b05415b858070cb1fad24ca
Chamod Rashmith <chamodrashmithpramuditha@gmail.com>
2026-08-14 06:15:06 +0530

    chore: bump compileSdk to 37 to fix flutter_secure_storage requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "lk.cmd.simple.todo"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
